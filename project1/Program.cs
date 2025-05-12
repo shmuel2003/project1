13ba936bd7782ac13c58d780ea8671af7a1d33a4
shmuelgross <shmuelgross3@gmail.com>
2025-05-12 17:33:27 +0300

    first

--- project1/Program.cs
+++ project1/Program.cs
@@ -10,7 +10,41 @@ namespace project1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("dff");
         }
+        static List<int> lst1 = new List<int>();
+        static void insertByUser()
+        {
+            int num;
+            do
+            {
+                Console.WriteLine("Enter at least 3 number, to stop press -1");
+                num = int.Parse(Console.ReadLine());
+                if (Positive(num))
+                {
+                    lst1.Add(num);
+                }
+            }
+            while (num != -1 || lst1.Count < 3);
+        }
+        static void printLst()
+        {
+            foreach (int i in lst1)
+            {
+                Console.Write(i);
+            }
+        }
+        static void printReverse()
+        {
+            for(int i=lst1.Count-1; i>=0; i--)
+            {
+                Console.Write(lst1[i]);
+            }
+        }
+
+        static bool Positive(int number)
+        {
+            return number >= 0;
+        }
+
     }
 }
