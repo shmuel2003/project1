fcdd3b6ec7cb63787058d2b26b3c2d77db11915f
shmuelgross <shmuelgross3@gmail.com>
2025-05-13 11:54:23 +0300

    functions

--- project1/Program.cs
+++ project1/Program.cs
@@ -9,36 +9,11 @@ namespace project1
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-        }
-        static List<int> lstUser = new List<int>();
         static bool Positive(int number)
         {
             return number >= 0;
         }
-        static void InsertByUser()
-        {
-            int num;
-            do
-            {
-                Console.WriteLine("Enter at least 3 number, to stop press -1");
-                num = int.Parse(Console.ReadLine());
-                if (Positive(num))
-                {
-                    lstUser.Add(num);
-                }
-            }
-            while (num != -1 || lstUser.Count < 3);
-        }
-        static void printLst()
-        {
-            foreach (int i in lstUser)
-            {
-                Console.Write(i+" ");
-            }
-        }
-        static List<int> ReverseList()
+        static List<int> ReverseList(List<int> lstUser)
         {
             List<int>lstReverse = new List<int>(lstUser);
             for (int i=lstUser.Count-1; i>=0; i--)
@@ -47,13 +22,13 @@ namespace project1
             }
             return lstReverse;
         }
-        static List<int>Sorted()
+        static List<int>Sorted(List<int> lstUser)
         {
             List<int>lstSort = new List<int> (lstUser);
             lstSort.Sort();
             return lstSort;
         }
-        static int MaxNum()
+        static int MaxNum(List<int> lstUser)
         {
             int numBig = 0;
             foreach(int number in lstUser)
@@ -65,7 +40,7 @@ namespace project1
             }
             return numBig;
         }
-        static int MinNum()
+        static int MinNum(List<int> lstUser)
         {
             int numMin = lstUser[0];
             for (int i = 1; i < lstUser.Count; i++)
@@ -77,20 +52,105 @@ namespace project1
             }
             return numMin;
         }
-        static int CountList()
+        static int CountList(List<int> lstUser)
         {
             int count = 0;
             foreach(int number in lstUser)
                 count++;
             return count;
         }
-        static int SumList()
+        static int SumList(List<int> lstUser)
         {
             int sum = 0;
             foreach (int number in lstUser)
                 sum += number;
             return sum;
         }
-
+        static float AvgList(List<int> lstUser)
+        {
+            return SumList(lstUser) / CountList(lstUser);
+        }
+        static void printLst(List<int> lstUser)
+        {
+            foreach (int i in lstUser)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        static void ChoiceUser(List<int> lstUser)
+        {
+            bool isProper = true;
+            while (isProper)
+            {
+                Console.WriteLine(
+                "a. Input a Series. (Replace the current series)\n" +
+                "b. Display the series in the order it was entered.\n" +
+                "c. Display the series in the reversed order it was entered.\n" +
+                "d. Display the series in sorted order (from low to high).\n" +
+                "e. Display the Max value of the series.\n" +
+                "f. Display the Min value of the series.\n" +
+                "g. Display the Average of the series.\n" +
+                "h. Display the Number of elements in the series.\n" +
+                "i. Display the Sum of the series.\n" +
+                "j. Exit.");
+                char choice = Console.ReadLine()[0];
+                switch (choice)
+                {
+                    case 'a':
+                        InsertByUser();
+                        break;
+                    case 'b':
+                        printLst(lstUser);
+                        break;
+                    case 'c':
+                        printLst(ReverseList(lstUser));
+                        break;
+                    case 'd':
+                        printLst(Sorted(lstUser));
+                        break;
+                    case 'e':
+                        Console.WriteLine(MaxNum(lstUser));
+                        break;
+                    case 'f':
+                        Console.WriteLine(MinNum(lstUser));
+                        break;
+                    case 'g':
+                        Console.WriteLine(AvgList(lstUser));
+                        break;
+                    case 'h':
+                        Console.WriteLine(CountList(lstUser));
+                        break;
+                    case 'i':
+                        Console.WriteLine(SumList(lstUser));
+                        break;
+                    case 'j':
+                        isProper = false;
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect choice");
+                        break;
+                }
+            }
+        }
+        static void InsertByUser()
+        {
+            List<int> lstUser = new List<int>();
+            int num;
+            do
+            {
+                Console.WriteLine("Enter at least 3 number, to stop press -1");
+                num = int.Parse(Console.ReadLine());
+                if (Positive(num))
+                {
+                    lstUser.Add(num);
+                }
+            }
+            while (num != -1 || lstUser.Count < 3);
+            ChoiceUser(lstUser);
+        }
+        static void Main(string[] args)
+        {
+            InsertByUser();
+        }
     }
 }
